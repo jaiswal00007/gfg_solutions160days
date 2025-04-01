484ac7b0e43b6927889f138eec754cf540bd216f
Anshu Jaiswal <137327313+jaiswal00007@users.noreply.github.com>
2025-04-01 13:15:27 +0530

    Create Move All Zeroes to End.java

---
+++ Move All Zeroes to End.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr);
+            int n = arr.length;
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int[] k= new int[arr.length];
+        int idx=0;
+        for (int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                k[idx++]=arr[i];
+            }
+        }
+        while(idx<arr.length){
+            k[idx++]=0;
+        }
+        for(int m=0;m<arr.length;m++){
+            arr[m]=k[m];
+        }
+        //optimal solution o(N) not working while compiling
+        
+        // int idx=0;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]!=0){
+        //         int temp=arr[i];
+        //         arr[i]=arr[idx];
+        //         arr[idx]=temp;
+        //         idx++;
+        //     }
+        // }
+        
+    
+        
+    
+    }
+    
+}
