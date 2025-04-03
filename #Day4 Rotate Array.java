f24adeb4a35208c01a12070e0d753de7aa533de2
Anshu Jaiswal <137327313+jaiswal00007@users.noreply.github.com>
2025-04-03 13:29:53 +0530

    Create #Day4 Rotate Array.java

---
+++ #Day4 Rotate Array.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        int len=arr.length;
+        if(len==0){
+            return;
+        }
+        d=d%len;
+        int temp[]=new int[len];
+        for(int i=0;i<len-d;i++){
+            temp[i]=arr[i+d];
+        }
+        for(int j=0;j<d;j++){
+            temp[len-d+j]=arr[j];
+        }
+        for(int k=0;k<len;k++){
+            arr[k]=temp[k];
+        }
+    
+    }
+    
+}
