e08196fb1a0a54b43ea99ee6cba9eaa6e06bfa65
Anshu Jaiswal <137327313+jaiswal00007@users.noreply.github.com>
2025-05-11 12:02:31 +0530

    Create #Day42 Two Sum - Pair with Given Sum.java

---
+++ #Day42 Two Sum - Pair with Given Sum.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            // int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[0]);
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            boolean ans = new Solution().twoSum(arr, x);
+            System.out.println(ans ? "true" : "false");
+            // System.out.println("~");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        Set<Integer> h= new HashSet<>();
+        // O(n) solution using HashSet
+        for(int n:arr){
+            int el= (target-n);
+            if(h.contains(el)){
+                return true;
+            }
+            h.add(n);
+        }
+        return false;
+    }
+}
