a431bafd46fed344c87c94699d167f0a2970dbdc
Anshu Jaiswal <137327313+jaiswal00007@users.noreply.github.com>
2025-05-24 18:48:17 +0530

    Create #Day55 Count the number of possible triangles.java

---
+++ #Day55 Count the number of possible triangles.java
@@ -0,0 +1,27 @@
+// User function Template for Java
+
+class Solution {
+    // Function to count the number of possible triangles.
+    static int countTriangles(int arr[]) {
+        // code here
+        int n=arr.length;
+        Arrays.sort(arr);
+        int count=0;
+        
+        for(int k=n-1;k>=2;k--){
+            int i=0;
+            int j=k-1;
+            
+            while(i<j){
+                if(arr[i]+arr[j]>arr[k]){
+                    count+=(j-i);
+                    j--;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+}
