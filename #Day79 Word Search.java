5cf8977961351f9c65baa94a32bac140e9ca3e0d
Anshu Jaiswal <137327313+jaiswal00007@users.noreply.github.com>
2025-06-17 12:38:09 +0530

    Create #Day79 Word Search.java

---
+++ #Day79 Word Search.java
@@ -0,0 +1,40 @@
+class Solution {
+    static public boolean isWordExist(char[][] mat, String word) {
+        // Code here
+        int n=mat.length;
+        int m=mat[0].length;
+        
+        boolean[] [] visited =new boolean[n][m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==word.charAt(0)){
+                    if(dfs(mat,word,i,j,0,visited)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+        
+    }
+    static boolean dfs(char[] [] mat,String word,int i,int j, int index,boolean[] [] visited){
+        if(index==word.length()){
+            return true;
+        }
+        if(i<0 || j<0 || i>=mat.length || j>=mat[0].length ||
+        visited[i][j] || mat[i][j]!=word.charAt(index)){
+            return false;
+        }
+        visited[i][j]=true;
+        
+        boolean found=  dfs(mat,word,i+1,j,index+1,visited) ||
+                        dfs(mat,word,i-1,j,index+1,visited) ||
+                        dfs(mat,word,i,j+1,index+1,visited) ||
+                        dfs(mat,word,i,j-1,index+1,visited);
+            
+        visited[i][j]=false;
+        return found;
+        
+    }
+}
